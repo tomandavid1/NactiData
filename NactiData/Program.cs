513572b863f42305ccc9dcfb7f1b9bec00b531c0
Pruone <tomandavid1@seznam.cz>
2023-06-27 17:30:04 +0200

    Add project files.

---
+++ NactiData/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace NactiData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vypocet vypocet = new Vypocet();
+
+            // Životní cyklus programu
+            while (true)
+            {
+                // Logika zpracování vstupů společně s validací
+                Console.WriteLine("Zadejte tři čísla: ");
+                double a = ValidaceVstupu();
+                double b = ValidaceVstupu();
+                double c = ValidaceVstupu();
+
+                // Výpis operací
+                Console.WriteLine("Součet čísel: " + vypocet.SoucetCisel(a, b, c));
+                Console.WriteLine("Součin čísel: " + vypocet.SoucinCisel(a, b, c));
+                Console.WriteLine("Součet prvních dvou čísel dělený třetím: " + vypocet.VlastniOperaceCisel(a, b, c));
+                Console.WriteLine("\nChcete spustit další výpočet? (ano/ne)");
+                
+                // Dotaz na opakování životního cyklu
+                string continueInput = Console.ReadLine().ToLower();
+                if (continueInput != "ano")
+                    break;
+                Console.Clear();
+            }
+
+            double ValidaceVstupu()
+            {
+                double vystup;
+                bool validniVstup = false;
+                do
+                {
+                    string vstup = Console.ReadLine().Trim();
+
+                    if (!double.TryParse(vstup, out vystup) || string.IsNullOrEmpty(vstup))
+                        Console.WriteLine("Toto není platné číslo! Opakujte vstup");
+                    else
+                        validniVstup = true;
+                } 
+                while (!validniVstup);
+
+                return vystup;
+
+            }
+        }
+    }
+}
---
+++ NactiData/Vypocet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NactiData
+{
+    class Vypocet
+    {
+        public double SoucetCisel(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        public double SoucinCisel(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+        public double VlastniOperaceCisel(double a, double b, double c)
+        {
+            return (a + b) / c;
+        }
+    }
+}
